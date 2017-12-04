651005afdee9089cac0a257bef842d8d6728c9dc
jinbh <jinbh@asiainfo.com>
2017-12-04 13:04:36 +0800

    EDAS部署设置

--- aibi-project-cp-acrm/src/main/java/com/asiainfo/cp/acrm/AcrmApplication.java
+++ aibi-project-cp-acrm/src/main/java/com/asiainfo/cp/acrm/AcrmApplication.java
@@ -1,15 +1,23 @@
 package com.asiainfo.cp.acrm;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-public class AcrmApplication {
+@EnableAutoConfiguration
+public class AcrmApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(AcrmApplication.class).web(true).run(args);
 	}
-
+	
+    @Override  
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {  
+    		builder.sources(this.getClass());
+        return super.configure(builder);  
+    }  
 }
