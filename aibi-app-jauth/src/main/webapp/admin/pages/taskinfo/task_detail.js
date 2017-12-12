38d50222cb0a9661ed8712afcd7d90df4221761c
张楠 <zhangnan7@asiainfo.com>
2017-12-12 10:30:38 +0800

    调度明细

--- aibi-app-jauth/src/main/webapp/admin/pages/taskinfo/task_detail.js
+++ aibi-app-jauth/src/main/webapp/admin/pages/taskinfo/task_detail.js
@@ -19,16 +19,54 @@ window.jauth_onload = function() {
 			})
 		}
 	})
-//	var urlShow = $.ctx + '';
-//	var colNames = [ ];
-//	var colModel = [ ];
-//	$("#mainGrid").jqGrid({
-//		url : urlShow,
-//		colNames : colNames,
-//		colModel : colModel,
-//		rownumbers : true,
-//		autowidth : true,
-//		viewrecords : true,
-//		pager : '#mainGridPager'
-//	});
+	var urlShow = $.ctx + '/api/taskexecute/taskexecutePage/query';
+	var colNames = [ '执行时间', '执行方式', '响应状态', '响应信息', '执行人' ];
+	var colModel = [ {
+		name : 'startTime',
+		index : 'startTime',
+		width : 10,
+		align : 'center',
+		formatter : function(cellvalue) {  
+			 return cellvalue.substr(0,19);
+		}
+	}, {
+		name : 'exeType',
+		index : 'exeType',
+		width : 10,
+		align : 'center'
+	}, {
+		name : 'status',
+		index : 'status',
+		width : 5,
+		align : 'center',
+	    formatter:function(v){return $.getCodeDesc('STATUS',v);}
+	}, {
+		name : 'returnMsg',
+		index : 'returnMsg',
+		width : 10,
+		align : 'center'
+	}, {
+		name : 'userId',
+		index : 'userId',
+		fixed : true,
+		width : 110,
+		align : 'center'
+	} ];
+	$("#mainGrid").jqGrid({
+		url : urlShow,
+		colNames : colNames,
+		colModel : colModel,
+		rownumbers : true,
+		autowidth : true,
+		viewrecords : true,
+		pager : '#mainGridPager'
+	});
+	// 查询按钮
+	$("#btnSearch").click(function() {
+		$("#mainGrid").setGridParam({
+			postData : $("#formSearch").formToJson()
+		}).trigger("reloadGrid", [{
+			page : 1
+		}]);
+	});
 }
