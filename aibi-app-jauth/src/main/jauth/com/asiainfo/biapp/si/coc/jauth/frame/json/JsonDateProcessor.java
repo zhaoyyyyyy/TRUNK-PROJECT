0efa379d39b6a3d55ef161a07b079654e99b0703
jinbh <jinbh@asiainfo.com>
2017-12-15 14:53:48 +0800

    修复fortify High等级安全问题-Race Condition: Format Flaw

--- aibi-app-jauth/src/main/jauth/com/asiainfo/biapp/si/coc/jauth/frame/json/JsonDateProcessor.java
+++ aibi-app-jauth/src/main/jauth/com/asiainfo/biapp/si/coc/jauth/frame/json/JsonDateProcessor.java
@@ -1,8 +1,8 @@
 package com.asiainfo.biapp.si.coc.jauth.frame.json;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.Date;
+
+import org.apache.commons.lang.time.FastDateFormat;
 
 import net.sf.json.JsonConfig;
 import net.sf.json.processors.JsonValueProcessor;
@@ -11,11 +11,11 @@ import net.sf.json.processors.JsonValueProcessor;
 public class JsonDateProcessor
   implements JsonValueProcessor
 {
-  private DateFormat format;
+  private FastDateFormat format;
 
   public JsonDateProcessor(String format)
   {
-    this.format = new SimpleDateFormat(format);
+    this.format =FastDateFormat.getInstance(format);
   }
 
   public Object processArrayValue(Object value, JsonConfig jsonConfig) {
