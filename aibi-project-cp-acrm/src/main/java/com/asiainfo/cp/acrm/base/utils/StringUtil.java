f85b3b1d21b6bd54b0b5dc37ed7747ea17734393
jinbh <jinbh@asiainfo.com>
2018-01-11 20:28:40 +0800

    #增加byteArray2HexStr方法

--- aibi-project-cp-acrm/src/main/java/com/asiainfo/cp/acrm/base/utils/StringUtil.java
+++ aibi-project-cp-acrm/src/main/java/com/asiainfo/cp/acrm/base/utils/StringUtil.java
@@ -31,4 +31,25 @@ public class StringUtil  extends StringUtils{
 	  {
 	    return StringUtils.splitPreserveAllTokens(src, flag);
 	  }
+	  
+	  /**
+		 * 将字符串的byte数组转换成十六进制串
+		 * 
+		 * @param data
+		 *            byte数组
+		 * @return 十六进制字符串
+		 * @author miaofc
+		 * @date Nov 28, 2011 4:40:26 PM
+		 */
+		public static String byteArray2HexStr(byte[] data) {
+			char[] digits = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+			int l = data.length;
+			char[] out = new char[l << 1];
+			// two characters form the hex value.
+			for (int i = 0, j = 0; i < l; i++) {
+				out[j++] = digits[(0xF0 & data[i]) >>> 4];
+				out[j++] = digits[0x0F & data[i]];
+			}
+			return new String(out);
+		}
 }
