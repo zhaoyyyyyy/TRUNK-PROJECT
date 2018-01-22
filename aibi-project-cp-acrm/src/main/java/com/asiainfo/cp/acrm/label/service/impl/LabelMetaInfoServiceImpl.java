e6355b777d05464ae38d57c5a1a34a561c387c94
jinbh <jinbh@asiainfo.com>
2018-01-22 15:27:25 +0800

    #去掉无用引用

--- aibi-project-cp-acrm/src/main/java/com/asiainfo/cp/acrm/label/service/impl/LabelMetaInfoServiceImpl.java
+++ aibi-project-cp-acrm/src/main/java/com/asiainfo/cp/acrm/label/service/impl/LabelMetaInfoServiceImpl.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.lang.math.RandomUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
