06e6f201a7808a31ea70ce98065f953be2b02409
zhougz3 <zhougz3@asiainfo.com>
2018-02-10 23:15:25 +0800

    1. 自己监控自己 2. 能跟较为方便的切换jar包 war包 3. 集成运行监控

--- aibi-app-jauth/src/main/jauth/com/asiainfo/biapp/si/coc/JauthApplication.java
+++ aibi-app-jauth/src/main/jauth/com/asiainfo/biapp/si/coc/JauthApplication.java
@@ -1,9 +1,10 @@
 package com.asiainfo.biapp.si.coc;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 import de.codecentric.boot.admin.config.EnableAdminServer;
@@ -19,13 +20,13 @@ import de.codecentric.boot.admin.config.EnableAdminServer;
 @EnableEurekaServer  //服务发现
 @EnableAdminServer
 @EnableConfigurationProperties
-public class JauthApplication {
+public class JauthApplication extends SpringBootServletInitializer {
 	
-//    @Override  
-//    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {  
-//    		builder.sources(this.getClass());
-//        return super.configure(builder);  
-//    } 
+    @Override  
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {  
+    		builder.sources(this.getClass());
+        return super.configure(builder);  
+    } 
     
 	public static void main(String[] args) {
 		SpringApplication.run(JauthApplication.class, args);
