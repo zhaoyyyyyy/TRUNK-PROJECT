4acaa114e0fbc2075d999df52eb43244af94479c
zhougz3 <zhougz3@asiainfo.com>
2018-02-27 21:45:11 +0800

    单点登录改造

--- aibi-project-cp-acrm/src/main/java/com/asiainfo/cp/acrm/AcrmApplication.java
+++ aibi-project-cp-acrm/src/main/java/com/asiainfo/cp/acrm/AcrmApplication.java
@@ -29,26 +29,26 @@ public class AcrmApplication extends SpringBootServletInitializer{
 	 * 配置过滤器
 	 * @return
 	 */
-	@Bean
-	public FilterRegistrationBean someFilterRegistration() {
-		FilterRegistrationBean registration = new FilterRegistrationBean();
-		registration.addUrlPatterns("/api/sso/*");
-		registration.addInitParameter("impl-classname", "com.ai.sso.external.DefaultPopedomImpl");
-		registration.addInitParameter("ALLOWPATH","gif;jpg;jpeg;png");
-		registration.setEnabled(true);
-		registration.setName("ssoFilter");
-		registration.setFilter(ssoFilter());
-		registration.setOrder(Integer.MIN_VALUE);//控制filter执行顺序,值越小越靠前执行
-		return registration;
-	}
+//	@Bean
+//	public FilterRegistrationBean someFilterRegistration() {
+//		FilterRegistrationBean registration = new FilterRegistrationBean();
+//		registration.addUrlPatterns("/api/sso/*");
+//		registration.addInitParameter("impl-classname", "com.ai.sso.external.DefaultPopedomImpl");
+//		registration.addInitParameter("ALLOWPATH","gif;jpg;jpeg;png");
+//		registration.setEnabled(true);
+//		registration.setName("ssoFilter");
+//		registration.setFilter(ssoFilter());
+//		registration.setOrder(Integer.MIN_VALUE);//控制filter执行顺序,值越小越靠前执行
+//		return registration;
+//	}
 
-	/**
-	 * 创建一个bean
-	 * @return
-	 */
-	@Bean(name = "ssoFilter")
-	public Test2Filter ssoFilter() {
-		return new Test2Filter();
-	}
+//	/**
+//	 * 创建一个bean
+//	 * @return
+//	 */
+//	@Bean(name = "ssoFilter")
+//	public Test2Filter ssoFilter() {
+//		return new Test2Filter();
+//	}
 	
 }
--- aibi-project-cp-acrm/src/main/java/com/asiainfo/cp/acrm/auth/filter/TokenAuthFilter.java
+++ aibi-project-cp-acrm/src/main/java/com/asiainfo/cp/acrm/auth/filter/TokenAuthFilter.java
@@ -54,7 +54,7 @@ public class TokenAuthFilter implements Filter {
     	response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));  
     	response.setHeader("Access-Control-Allow-Credentials", "true");
     	
-    	response.setHeader("Access-Control-Allow-Origin", "http://crm.chinapost.com:8444");  
+    	//response.setHeader("Access-Control-Allow-Origin", "http://crm.chinapost.com:8441");  
     	response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");  
     	response.setHeader("Access-Control-Max-Age", "3600");  
     	response.setHeader("Access-Control-Allow-Headers", "");
